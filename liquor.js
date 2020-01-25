29dcb779f795aaf0097452a21556c40ecabf09c9
Harvey Le <harveyxcle@gmail.com>
2020-01-25 17:00:39 -0500

    trying to target individual links

--- liquor.js
+++ liquor.js
@@ -6,35 +6,45 @@
 //   );
 // }
 
-// function fetchRecipe(id) {
+// function fetchUrl(id) {
 //   const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + id;
-
-//   fetch(url)
-//     .then(response => response.json())
-//     .then(responseJson => displayRecipe(responseJson));
+//   return url;
 // }
 
 function displayLiquorList(responseJson) {
+  const idStore = [];
   console.log(responseJson);
 
   for (let i = 0; i < responseJson.drinks.length; i++) {
-    // const id = responseJson.drinks[i].idDrink;
-    // console.log(id);
+    let id = responseJson.drinks[i].idDrink;
 
     $('#liquor-list').append(
-      `<a href="#" class="liquor-link" title="${responseJson.drinks[i].idDrink}"><li class="liquor-li"><img src="${responseJson.drinks[i].strDrinkThumb}" alt="${responseJson.drinks[i].strDrink}" class="recipe-list-img" />
-    <h3>${responseJson.drinks[i].strDrink}</h3></li></a>`
+      `<a href="#" class="${responseJson.drinks[i].idDrink}">
+      <li class="liquor-li">
+      <img src="${responseJson.drinks[i].strDrinkThumb}" alt="${responseJson.drinks[i].strDrink}" class="recipe-list-img" />
+      <h3>${responseJson.drinks[i].strDrink}</h3></li></a>`
     );
-    console.log();
+    idStore.push(responseJson.drinks[i].idDrink);
+    findClass(id);
   }
-  findId();
+
+  console.log(idStore);
 }
 
-function findId() {
-  $('.liquor-link').click(event => {
-    const id = $('.liquor-link').attr('title');
-    console.log(id);
-  });
+function findClass(id) {
+  console.log(id);
+  let x = '.' + id;
+  $('x').attr('title', 'active');
+  // $('#liquor-list').on('click', 'a', event => {
+  //   $('a').attr('title', 'active');
+  // });
+}
+
+function fetchRecipe(url) {
+  console.log(url);
+  fetch(url)
+    .then(response => response.json())
+    .then(responseJson => displayRecipe(responseJson));
 }
 
 function pullList(liquor) {
